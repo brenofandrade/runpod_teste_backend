54e83e855dd9ef3dfa1612119486a57af0b835eb
Breno Andrade <andradefbreno@gmail.com>
2025-08-29 10:09:19 -0300

    update file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+# from flask_cors import CORS
 app = Flask(__name__)
-CORS(app) 
+# CORS(app) 
 
 @app.get("/health")
 def check_health():
